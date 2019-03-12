694a8676993bfe1df2e46bac06ed7cac01c7043f
veljko1992 <veljko.1992@gmail.com>
2019-03-12 14:52:55 +0100

    Code refactoring

--- js/templates/attributeBooksTemplate.js
+++ js/templates/attributeBooksTemplate.js
@@ -1,6 +1,7 @@
 $attributeBooksWrapper = $('#attributeBookItems');
 var attributeBooks = localStorage.getItem('attributeBooks');
 
+
 if(attributeBooks == 'new'){
     $('#attributeBooksHeading').html('NOVE KNJIGE');
 }
@@ -11,10 +12,7 @@ if(attributeBooks == 'sales'){
     $('#attributeBooksHeading').html('RASPRODAJA KNJIGA');
 }
 
-
-
-$.getJSON("../json/books.json", function (result) {
-    var attributeBook = result;
+function createBooks(attributeBook){
     var attributeItems = '';
 
     for(i = 0; i < attributeBook.length; i++){
@@ -31,9 +29,16 @@ $.getJSON("../json/books.json", function (result) {
                         </div>` 
         }
     }
-
     $attributeBooksWrapper.html(attributeItems);
+}
+
+
+$.getJSON("../json/books.json", function (result) {
+    var attributeBook = result;
+    createBooks(attributeBook);
+    
 });
+
 
 var $attributeSortItemsInput = $('#attributeSortItems');
 
@@ -48,21 +53,7 @@ $attributeSortItemsInput.on('change', function(){
         
             attributeBook = attributeBook.sort(function(a, b){return b.price - a.price});
             
-            for(i = 0; i < attributeBook.length; i++){
-                if(attributeBook[i].attribute == attributeBooks){
-                    attributeItems += `<div class="bookItem col-6 col-sm-4 col-lg-3 imgbox" data-name="${attributeBook[i].name}" data-price="${attributeBook[i].price}" data-id="${attributeBook[i].id}">
-                                <div class="imgWrapper"><a href="../pages/showBookPage.html" class="showItem"><img src="../img/${attributeBook[i].img}" alt="" class="img-fluid"></a></div>
-                                    <div class="textWrapper">
-                                      <ul>
-                                        <li class="itemName">${attributeBook[i].name}</li>
-                                        <li class="itemPrice">${attributeBook[i].price},00 RSD</li>
-                                        <li><a href="" class="addItem">Kupite</a></li>
-                                      </ul>
-                                    </div>
-                                </div>` 
-                }
-            }          
-            $attributeBooksWrapper.html(attributeItems);
+            createBooks(attributeBook);
         });
     }
 
@@ -74,21 +65,7 @@ $attributeSortItemsInput.on('change', function(){
             
             attributeBook = attributeBook.sort(function(a, b){return a.price - b.price});
             
-            for(i = 0; i < attributeBook.length; i++){
-                if(attributeBook[i].attribute == attributeBooks){
-                    attributeItems += `<div class="bookItem col-6 col-sm-4 col-lg-3 imgbox" data-name="${attributeBook[i].name}" data-price="${attributeBook[i].price}" data-id="${attributeBook[i].id}">
-                                <div class="imgWrapper"><a href="../pages/showBookPage.html" class="showItem"><img src="../img/${attributeBook[i].img}" alt="" class="img-fluid"></a></div>
-                                    <div class="textWrapper">
-                                      <ul>
-                                        <li class="itemName">${attributeBook[i].name}</li>
-                                        <li class="itemPrice">${attributeBook[i].price},00 RSD</li>
-                                        <li><a href="" class="addItem">Kupite</a></li>
-                                      </ul>
-                                    </div>
-                                </div>` 
-                }
-            }
-            $attributeBooksWrapper.html(attributeItems);
+            createBooks(attributeBook);
         });
     }
 });
--- js/templates/categoriesBooksTemplate.js
+++ js/templates/categoriesBooksTemplate.js
@@ -16,12 +16,9 @@ if(categoriesBooks == 'computer'){
     $('#categoriesBooksHeading').html('KOMPJUTERSKA LITERATURA');
 }
 
-
-
-$.getJSON("../json/books.json", function (result) {
+function createBooks(categoriesBook){
     var categoriesItems = '';
-    var categoriesBook = result;
-  
+
     for(i = 0; i < categoriesBook.length; i++){
         if(categoriesBook[i].categories == categoriesBooks){
             categoriesItems += `<div class="bookItem col-6 col-sm-4 col-lg-3 imgbox" data-name="${categoriesBook[i].name}" data-price="${categoriesBook[i].price}" data-id="${categoriesBook[i].id}">
@@ -38,6 +35,13 @@ $.getJSON("../json/books.json", function (result) {
     }
 
     $categoriesBooksWrapper.html(categoriesItems);
+}
+
+
+
+$.getJSON("../json/books.json", function (result) {
+    var categoriesBook = result;
+    createBooks(categoriesBook);
 });
 
 var $categoriesSortItemsInput = $('#categoriesSortItems');
@@ -48,52 +52,18 @@ $categoriesSortItemsInput.on('change', function(){
     if($categoriesSortItemsInput.val() == 'descending'){
         
         $.getJSON("../json/books.json", function (result) {
-            var categoriesItems = '';
             var categoriesBook = result;
-        
             categoriesBook = categoriesBook.sort(function(a, b){return b.price - a.price});
-            
-            for(i = 0; i < categoriesBook.length; i++){
-                if(categoriesBook[i].categories == categoriesBooks){
-                    categoriesItems += `<div class="bookItem col-6 col-sm-4 col-lg-3 imgbox" data-name="${categoriesBook[i].name}" data-price="${categoriesBook[i].price}" data-id="${categoriesBook[i].id}">
-                                <div class="imgWrapper"><a href="../pages/showBookPage.html" class="showItem"><img src="../img/${categoriesBook[i].img}" alt="" class="img-fluid"></a></div>
-                                    <div class="textWrapper">
-                                      <ul>
-                                        <li class="itemName">${categoriesBook[i].name}</li>
-                                        <li class="itemPrice">${categoriesBook[i].price},00 RSD</li>
-                                        <li><a href="" class="addItem">Kupite</a></li>
-                                      </ul>
-                                    </div>
-                                </div>` 
-                }
-            }          
-            $categoriesBooksWrapper.html(categoriesItems);
+            createBooks(categoriesBook);
         });
     }
 
     if($categoriesSortItemsInput.val() == 'ascending'){
 
         $.getJSON("../json/books.json", function (result) {
-            var categoriesItems = '';
             var categoriesBook = result;
-            
             categoriesBook = categoriesBook.sort(function(a, b){return a.price - b.price});
-            
-            for(i = 0; i < categoriesBook.length; i++){
-                if(categoriesBook[i].categories == categoriesBooks){
-                    categoriesItems += `<div class="bookItem col-6 col-sm-4 col-lg-3 imgbox" data-name="${categoriesBook[i].name}" data-price="${categoriesBook[i].price}" data-id="${categoriesBook[i].id}">
-                                <div class="imgWrapper"><a href="../pages/showBookPage.html" class="showItem"><img src="../img/${categoriesBook[i].img}" alt="" class="img-fluid"></a></div>
-                                    <div class="textWrapper">
-                                      <ul>
-                                        <li class="itemName">${categoriesBook[i].name}</li>
-                                        <li class="itemPrice">${categoriesBook[i].price},00 RSD</li>
-                                        <li><a href="" class="addItem">Kupite</a></li>
-                                      </ul>
-                                    </div>
-                                </div>` 
-                }
-            }
-            $categoriesBooksWrapper.html(categoriesItems);
+            createBooks(categoriesBook);
         });
     }
 });
