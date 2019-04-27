d599d16745f9ccaa1293f061b93efe21c2e77d00
veljko <veljko.1992@gmail.com>
2019-04-27 11:02:27 +0200

    Create PWA

--- sw.js
+++ sw.js
@@ -1,31 +1,34 @@
-self.addEventListener('install', function (event) {
-    console.log('SW Installed');
-    event.waitUntil(
-        caches.open('static')
-            .then(function (cache) {
-                // cache.add('/');
-                // cache.add('/index.html');
-                // cache.add('/src/js/app.js');
-                cache.addAll([
-                    'index.html'
-                ]);
-            })
-    );
-});
+/* eslint-disable no-console */
 
-self.addEventListener('activate', function () {
-    console.log('SW Activated');
-});
+import { register } from "register-service-worker";
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (res) {
-                if (res) {
-                    return res;
-                } else {
-                    return fetch(event.request);
-                }
-            })
-    );
-});
+if (process.env.NODE_ENV === "production") {
+    register(`${process.env.BASE_URL}service-worker.js`, {
+        ready() {
+            console.log(
+                "App is being served from cache by a service worker.\n" +
+                "For more details, visit https://goo.gl/AFskqB"
+            );
+        },
+        registered() {
+            console.log("Service worker has been registered.");
+        },
+        cached() {
+            console.log("Content has been cached for offline use.");
+        },
+        updatefound() {
+            console.log("New content is downloading.");
+        },
+        updated() {
+            console.log("New content is available; please refresh.");
+        },
+        offline() {
+            console.log(
+                "No internet connection found. App is running in offline mode."
+            );
+        },
+        error(error) {
+            console.error("Error during service worker registration:", error);
+        }
+    });
+}
